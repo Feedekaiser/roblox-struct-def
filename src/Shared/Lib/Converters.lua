75cb6f7d5f170e9b2ff1140374a0236d9c414cb5
Feedekaiser <77873991+Feedekaiser@users.noreply.github.com>
2021-03-29 01:36:39 +0800

    added datetime and brickcolor

--- src/Shared/Lib/Converters.lua
+++ src/Shared/Lib/Converters.lua
@@ -2,6 +2,13 @@ local EMPTY_VEC2 = Vector2.new(0, 0)
 local EMPTY_VEC3 = Vector3.new(0, 0, 0)
 local EMPTY_REG3 = Region3.new(EMPTY_VEC3, EMPTY_VEC3)
 local EMPTY_COL3 = Color3.new(0, 0, 0)
+local EMPTY_DATE = DateTime.fromUnixTimestampMillis(0)
+local EMPTY_BCOL = BrickColor.new()
+
+local INDEX_META = {}
+INDEX_META.__index = function()
+	return 0;
+end
 
 local Converters = {
 	[Vector3] = {
@@ -16,13 +23,13 @@ local Converters = {
 				local out = {}
 
 				if typeof(value) ~= 'Vector3' then
-					out[1] = 0
-					out[2] = 0
-					out[3] = 0
+					out[#out+1] = 0
+					out[#out+1] = 0
+					out[#out+1] = 0
 				else
-					out[4] = value.X
-					out[5] = value.Y
-					out[6] = value.Z
+					out[#out+1] = value.X
+					out[#out+1] = value.Y
+					out[#out+1] = value.Z
 				end
 
 				return out
@@ -73,169 +80,6 @@ local Converters = {
 			end
 		}
 	},
-	[Region3] = {
-		{
-			-- type
-			'double[]',
-			-- default
-			EMPTY_REG3,
-			-- To Serialize
-			function (schema, field, value)
-				local out = {}
-
-				if typeof(value) ~= 'Region3' then
-					out[1] = 0
-					out[2] = 0
-					out[3] = 0
-					out[4] = 0
-					out[5] = 0
-					out[6] = 0
-				else
-					local x1, y1, z1, x2, y2, z2
-
-					do
-						local Size = value.Size
-						local CFrame = value.CFrame
-
-						x1, y1, z1 = CFrame.X, CFrame.Y, CFrame.Z
-						x2, y2, z2 = Size.X/2, Size.Y/2, Size.Z/2
-					end
-
-					out[1] = x1-x2
-					out[2] = y1-y2
-					out[3] = z1-z2
-					out[4] = x1+x2
-					out[5] = y1+y2
-					out[6] = z1+z2
-				end
-
-				return out
-			end,
-			-- To Instance
-			function(schema, field, value)
-				if value == nil then
-					return EMPTY_VEC3
-				end
-				return Region3.new(Vector3.new(value[1], value[2], value[3]), Vector3.new(value[4], value[5], value[6]))
-			end
-		},
-		{
-			-- type
-			'double[]',
-			-- default
-			{},
-			-- To Serialize
-			function(schema, field, value)
-				local out = {}
-
-				for _, reg3 in ipairs(value) do
-					if typeof(reg3) ~= 'Region3' then
-						out[#out+1] = 0
-						out[#out+1] = 0
-						out[#out+1] = 0
-						out[#out+1] = 0
-						out[#out+1] = 0
-						out[#out+1] = 0
-					else
-						local x1, y1, z1, x2, y2, z2
-
-						do
-							local Size = value.Size
-							local CFrame = value.CFrame
-
-							x1, y1, z1 = CFrame.X, CFrame.Y, CFrame.Z
-							x2, y2, z2 = Size.X/2, Size.Y/2, Size.Z/2
-						end
-
-						out[#out+1] = x1-x2
-						out[#out+1] = y1-y2
-						out[#out+1] = z1-z2
-						out[#out+1] = x1+x2
-						out[#out+1] = y1+y2
-						out[#out+1] = z1+z2
-					end
-				end
-
-				return out
-			end,
-			-- To Instance
-			function(schema, field, value)
-				local out = {}
-				if value == nil or #value == 0 then
-					return out
-				end
-
-				for i = 1, #value, 6 do
-					out[#out+1] = Region3.new(Vector3.new(value[i], value[i+1], value[i+2]), Vector3.new(value[i+3], value[i+4], value[i+5]))
-				end
-				return out
-			end
-		}
-	},
-	[Vector2] = {
-		-- Single
-		{
-			-- type
-			'double[]',
-			-- default
-			EMPTY_VEC2,
-			-- To Serialize
-			function (schema, field, value)
-				local out = {}
-
-				if typeof(value) ~= 'Vector2' then
-					out[#out+1] = 0
-					out[#out+1] = 0
-				else
-					out[#out+1] = value.X
-					out[#out+1] = value.Y
-				end
-
-				return out
-			end,
-			-- To Instance
-			function(schema, field, value)
-				if value == nil then
-					return EMPTY_VEC2
-				end
-				return Vector3.new(value[1], value[2])
-			end
-		},
-		{
-			-- type
-			'double[]',
-			-- default
-			{},
-			-- To Serialize
-			function(schema, field, value)
-				local out = {}
-
-				for _, vec2 in ipairs(value) do
-					if typeof(vec2) ~= 'Vector2' then
-						out[#out+1] = 0
-						out[#out+1] = 0
-					else
-						out[#out+1] = vec2.X
-						out[#out+1] = vec2.Y
-					end
-				end
-
-				return out
-			end,
-			-- To Instance
-			function(schema, field, value)
-				local out = {}
-				if value == nil or #value == 0 then
-					return out
-				end
-
-				for i = 1, #value, 2 do
-					out[#out+1] = Vector3.new(value[i], value[i+1])
-				end
-				return out
-			end
-		}
-	},
 	[Color3] = {
 		--Single
 		{
@@ -245,7 +89,7 @@ local Converters = {
 			EMPTY_COL3,
 			-- To Serialize
 			function (schema, field, value)
-				--// 255 (since Color3.R returns a scalar) * {2^16, 2^8, 2^0}
+				--// 255 * {2^16, 2^8, 2^0}
 				return bit32.bor(value.R * 0xFF0000, value.G * 0xFF00, value.B * 0xFF)
 			end,
 			-- To Instance
@@ -290,7 +134,113 @@ local Converters = {
 				return out
 			end
 		}
+	},
+	[DateTime] = {
+		{
+			-- Single
+			'int53',
+			EMPTY_DATE,
+			-- To Serialize
+			function (schema, field, value)
+				return value.UnixTimestampMillis
+			end,
+			-- To Instance
+			function(schema, field, value)
+				if value == nil then
+					return EMPTY_DATE
+				end
+				return DateTime.fromUnixTimestampMillis(value)
+			end
+		},
+		{
+			'int53[]',
+			{},
+			function (schema, field, value)
+				local out = {}
+
+				for _, v in ipairs(value) do
+					out[#out+1] = v.UnixTimestampMillis
+				end
+
+				return out
+			end,
+			function(schema, field, value)
+				local out = {}
+				if value == nil or #value == 0 then
+					return out
+				end
+
+				for _, n in ipairs(value) do
+					out[#out+1] = DateTime.fromUnixTimestampMillis(n)
+				end
+				return out
+			end
+		}
+	},
+	[BrickColor] = {
+		{
+			'int32',
+			EMPTY_BCOL,
+			function (schema, field, value)
+				return value.Number
+			end,
+			function (schema, field, value)
+				return value and BrickColor.new(value) or EMPTY_BCOL
+			end
+		},
+		{
+			'int53[]',
+			{},
+			function (schema, field, value)
+				local out = {}
+
+				--// Since the highest value possible is 1032, which uses 11 bits, we can only store 4 BrickColor in 53 bits.
+				--// Same algorithm as the RGB conversion.
+
+				setmetatable(value, INDEX_META) --// Setting it to INDEX_META (which makes indexing empty field return 0 instead of raising an error.) to avoid long ternary statement
+
+				for i = 1, #value, 4 do --// 2^33, 2^22, 2^11
+					out[i] = value[i+3].Number * 8589934592 + value[i+2].Number * 4194304 + value[i+1].Number * 2048 + value[i].Number
+				end
+
+				setmetatable(value, nil)
+
+				return out
+			end,
+			function (schema, field, value)
+				local out = {}
+
+				if value == nil or #value == 0 then
+					return out
+				end
+
+				--// BrickColor index starts at 1, so we can assume 0 as it doesnt contain the value.
+				local Decode = function(n, d)
+					local Result = bit32.band(n/d, 0x7FF)
+
+					if Result == 0 then
+						return
+					end
+
+					out[#out+1] = BrickColor.new(Result)
+
+					return true
+				end
+
+				for _, n in ipairs(value) do
+					out[#out+1] = BrickColor.new(bit32.band(n, 0x7FF))
+
+					--// Lazy evaluation made this work. (Kinda abuse)
+					if Decode(n, 2048) and Decode(n, 4194304) and Decode(n, 8589934592) then
+
+					end
+				end
+
+				return out
+			end
+		}
 	}
 }
+
 
 return Converters
